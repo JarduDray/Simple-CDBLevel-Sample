f0c8bd614aaab3e52091a84a210ee1df11278a14
Brandon Dulaney <jardugames626@gmail.com>
2019-07-10 08:57:51 -0500

    Level Loading Code and Cleanup

--- Level.hx
+++ Level.hx
@@ -1,7 +1,24 @@
 package;
 
+import h2d.Object;
+import cdb.Types.Index;
 import h2d.CdbLevel;
 
 class Level extends CdbLevel {
-    
+	/**
+	 * Level constructor overriding CDBLevel constructor
+	 * @param i - Instance of a single level from the CDB file
+	 * @param idx - Index of that level in the file
+	 * @param p - Optional parent Object can be added here. If you use Layers and want the whole level to be a part of that Layer,
+	 *            this is where you would connect it
+	 */
+	public override function new(i:Index<CdbLevel>, idx:Int, ?p:Object) {
+		super(i, idx, p);
+	}
+
+	/* Insert specific level code here. I'd highly recommend the following one at least
+        public function update(delta:Float32) {
+            // This method allows you to update input, ai, etc for the level
+        }
+    */
 }
--- Main.hx
+++ Main.hx
@@ -1,14 +1,50 @@
 package;
 
+import cdb.Types.Index;
+import cdb.Parser;
+import cdb.Data;
 import hxd.Res;
 import hxd.App;
+import h2d.CdbLevel;
 
 class Main extends App {
-	override function init() {}
+	static var s:String;
+
+	static var lvls:Array<Level>;
+
+	override function init() {
+		/**  
+		 * Create Data Object from CDB file
+		 * First input is String var created earlier
+		 * Second input is for editing purposes; if you use a custom editor, this would be set to true (need clarification)
+		 */
+		var d:Data = cdb.Parser.parse(s, false);
+
+		/**
+		 * Create instance of level (Index) using Data from last step and the specific name of the level you're loading
+		 */
+		var i:Index<CdbLevel> = new Index(d, "arena");
+
+		/**
+		 * Level Loading Magic
+		 */
+		var l:Level = new Level(i, 0, s2d);
+	}
 
 	static function main() {
+		// an array of levels loaded from CDB file
+		lvls = new Array();
+
+		// Very Important for loading Assets
 		hxd.Res.initEmbed();
-		TestDB.load(hxd.Res.test.entry.getText());
+
+		// helper var for CdbLevel loading
+		s = hxd.Res.test.entry.getText();
+
+		// Not as important for time being but does give better access to CDB file
+		TestDB.load(s);
+
+		// Start your engines
 		new Main();
 	}
 }
